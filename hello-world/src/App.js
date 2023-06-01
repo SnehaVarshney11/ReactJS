342c999e67157f0b3ec33e9ac45a1c6e6efa3b76
sneha <sneha.varshney_cs19@gla.ac.in>
2023-06-01 14:39:51 +0530

    About Class Component

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Greet from './components/Greet';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Welcome />
     </div>
   );
 }
---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,11 @@
+// Class Component
+import React, {Component} from 'react'
+
+//DEFINE CLASS
+class Welcome extends Component{
+    render() {
+        return <h1>Class Component</h1>
+    }
+}
+
+export default Welcome
