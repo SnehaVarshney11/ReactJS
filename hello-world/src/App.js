c3f6c2c3358bae68efef4f4e487f8cd7dbf16f4e
sneha <sneha.varshney_cs19@gla.ac.in>
2023-06-01 14:22:31 +0530

    Create Hello World Program

---
+++ hello-world/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Greet from './components/Greet';
+
+function App() {
+  return (
+    <div className="App">
+      <Greet />
+    </div>
+  );
+}
+
+export default App;
