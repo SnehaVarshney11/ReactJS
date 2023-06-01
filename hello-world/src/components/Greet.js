89bd0351c30fe3f160ace54199362c6dd05c249d
sneha <sneha.varshney_cs19@gla.ac.in>
2023-06-01 17:25:02 +0530

    Destructuring the props in Functional Component

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -5,7 +5,7 @@ import React from 'react'
 //     return <h1>Hello World!</h1>
 // }
 
-const Greet = props => {
+/*const Greet = props => {
     console.log(props)
     return (
         <div>
@@ -13,6 +13,26 @@ const Greet = props => {
             {props.children}
         </div> 
     )
-}   
+}  */
+
+//Destructuring the props
+// 1 method
+/* const Greet = ({name, heroName}) => {
+    return (
+        <div>
+            <h1>Hello {name} a.k.a {heroName}</h1>
+        </div> 
+    )
+} */
+
+// 2 method
+const Greet = props => {
+    const{name, heroName} = props
+    return (
+        <div>
+            <h1>Hello {name} a.k.a {heroName}</h1>
+        </div> 
+    )
+} 
 
 export default Greet
