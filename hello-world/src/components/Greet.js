b6c9b20a901893026fb7d9c6d7de2220cafe4743
sneha <sneha.varshney_cs19@gla.ac.in>
2023-06-01 14:40:05 +0530

    About Functional Component

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,7 +1,10 @@
+// Functional Component 
 import React from 'react'
 
-function Greet() {
-    return <h1>Hello World!</h1>
-}
+// function Greet() {
+//     return <h1>Hello World!</h1>
+// }
+
+const Greet = () => <h1>Hello World!</h1>
 
 export default Greet
