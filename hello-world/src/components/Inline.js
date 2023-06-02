84af03529a99650473dc3d91b218566f28a1b6ab
sneha <sneha.varshney_cs19@gla.ac.in>
2023-06-02 18:34:17 +0530

    About Inline Styling

---
+++ hello-world/src/components/Inline.js
@@ -0,0 +1,17 @@
+//FILE FOR INLINE STYLING
+import React from 'react'
+
+const heading = {
+    fontSize: '72px',
+    color: 'blue'
+}
+
+function Inline() {
+  return (
+    <div>
+        <h1 style={heading}>Inline Styling</h1>
+    </div>
+  )
+}
+
+export default Inline
