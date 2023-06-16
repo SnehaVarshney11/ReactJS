3070250c555dd7e2e9602b675b643d4f300bc990
Sneha Varshney <sneha@insurka.com>
2023-06-16 18:27:18 +0530

    About Error Boundary

---
+++ hello-world/src/components/ErrorBoundary.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+class ErrorBoundary extends Component {
+    constructor(props) {
+      super(props)
+    
+      this.state = {
+         hasError: false
+      }
+    }
+    static getDerivedStateFromError(error) {
+        return {
+            hasError: true
+        }
+    }
+  render() {
+    if(this.state.hasError) {
+        <h1>Something went wrong</h1>
+    }
+    return this.props.children
+  }
+}
+
+export default ErrorBoundary
