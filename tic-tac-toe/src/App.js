42e43d3883e762eb655b61d381c20cb647eb65eb
sneha <sneha.varshney_cs19@gla.ac.in>
2024-04-16 16:15:02 +0530

    Tic Tac Toe

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function Square({value, onSquareClick}) {
+  return (
+    <button className="square" onClick={onSquareClick}>{value}</button>
+  );
+}
+
+export default function Board() {
+
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  function handleClick(i) {
+    if(squares[i]) {
+      return;
+    }
+    const nextSquare = squares.slice();
+    if(xIsNext) {
+      nextSquare[i] ="X"
+    }else {
+      nextSquare[i] = "O";
+    }
+    setSquares(nextSquare);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <div className="board-container">
+      <div className="board">
+        <div className="board-row">
+          <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+          <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+          <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+          <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+          <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+        </div>
+        <div className="board-row">
+          <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+          <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+          <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+        </div>
+      </div>
+    </div>
+  );
+}
