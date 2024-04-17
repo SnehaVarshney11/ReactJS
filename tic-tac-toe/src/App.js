83660f94a2f787bded58e3e7dca89e5cdefd9719
sneha <sneha.varshney_cs19@gla.ac.in>
2024-04-17 12:03:52 +0530

    commit

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -13,7 +13,7 @@ export default function Board() {
   const [xIsNext, setXIsNext] = useState(true);
 
   function handleClick(i) {
-    if(squares[i]) {
+    if(squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquare = squares.slice();
@@ -26,9 +26,18 @@ export default function Board() {
     setXIsNext(!xIsNext);
   }
 
+  const winner = calculateWinner(squares);
+  let status;
+  if(winner) {
+    status = "Winner : " + winner;
+  } else {
+    status = "Next Player : " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <div className="board-container">
       <div className="board">
+        <div className='status'>{status}</div>
         <div className="board-row">
           <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
           <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
@@ -47,4 +56,27 @@ export default function Board() {
       </div>
     </div>
   );
+}
+
+
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  for(let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
