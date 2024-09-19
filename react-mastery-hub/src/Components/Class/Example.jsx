7d9dcdecd72752781c24d0f823e2030cf8faba6c
sneha <sneha.varshney_cs19@gla.ac.in>
2024-09-19 11:11:34 +0530

    Class Component

--- react-mastery-hub/src/Components/Class/Example.jsx
+++ react-mastery-hub/src/Components/Class/Example.jsx
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class MyClassComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            count: 0
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+    };
+  }
 
-    render() {
-        return <div>{this.state.count}</div>;
-    }
+  render() {
+    return (
+      <>
+        <h1>About Class Component</h1>
+        <div>{this.state.count}</div>
+      </>
+    );
+  }
 }
 
 export default MyClassComponent;
