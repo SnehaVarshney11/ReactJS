a9191da8be5d64d957f5ff38089c8d2b6485d095
sneha <sneha.varshney_cs19@gla.ac.in>
2024-09-19 12:37:43 +0530

    useState Hook

---
+++ react-mastery-hub/src/State Management/Counter.jsx
@@ -0,0 +1,16 @@
+// FILE FOR useState Hook
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
