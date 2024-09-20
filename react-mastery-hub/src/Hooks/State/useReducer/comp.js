43011a3880ce2dfff4188d7f31c74cac9e909b6a
sneha <sneha.varshney_cs19@gla.ac.in>
2024-09-20 12:36:21 +0530

    useReducer Hook

--- react-mastery-hub/src/Hooks/State/useReducer/comp.js
+++ react-mastery-hub/src/Hooks/State/useReducer/comp.js
@@ -1,45 +1,46 @@
-import React, { useReducer } from 'react';
-import { initialState, reducer } from './state';
+import React, { useReducer } from "react";
+import { initialState, reducer } from "./state";
 
 const MyComponent = () => {
-    const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-    const handleStart = () => {
-        dispatch({ type: 'START' });
-    }
+  const handleStart = () => {
+    dispatch({ type: "START" });
+  };
 
-    const handlePause = () => {
-        dispatch({ type: 'PAUSE' });
-    }
+  const handlePause = () => {
+    dispatch({ type: "PAUSE" });
+  };
 
-    const handleResume = () => {
-        dispatch({ type: 'RESUME' });
-    }
+  const handleResume = () => {
+    dispatch({ type: "RESUME" });
+  };
 
-    const handleStop = () => {
-        dispatch({ type: 'STOP' });
-    }
+  const handleStop = () => {
+    dispatch({ type: "STOP" });
+  };
 
-    return (
-        <>
-            <h2>Current State: {state.currState}</h2>
-            {state.currState === 'idle' && (
-                <button onClick={handleStart}>Start</button>
-            )}
-            {state.currState === 'running' && (
-                <div>
-                  <button onClick={handlePause}>Pause</button>
-                  <button onClick={handleStop}>Stop</button>
-                </div>
-            )}
-            {state.currState === 'paused' && (
-              <div>
-                <button onClick={handleResume}>Resume</button>
-                <button onClick={handleStop}>Stop</button>
-              </div>
-            )}
-        </>
-    )
+  return (
+    <>
+      <h1>Example of useReducer Hook</h1>
+      <h2>Current State: {state.currState}</h2>
+      {state.currState === "idle" && (
+        <button onClick={handleStart}>Start</button>
+      )}
+      {state.currState === "running" && (
+        <div>
+          <button onClick={handlePause}>Pause</button>
+          <button onClick={handleStop}>Stop</button>
+        </div>
+      )}
+      {state.currState === "paused" && (
+        <div>
+          <button onClick={handleResume}>Resume</button>
+          <button onClick={handleStop}>Stop</button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default MyComponent;
--- react-mastery-hub/src/Hooks/State/useReducer/state.js
+++ react-mastery-hub/src/Hooks/State/useReducer/state.js
@@ -1,35 +1,36 @@
 export const initialState = {
-    currState: 'idle'
-}
+  currState: "idle",
+};
 
+// reducer method
 export const reducer = (state, action) => {
-    switch(state.currState) {
-        case 'idle':
-            switch(action.type) {
-                case 'START':
-                    return {...state, currState: 'running'};
-                default:
-                    return state;
-            }
-        case 'running':
-            switch(action.type) {
-                case 'PAUSE':
-                    return {...state, currState: 'paused'};
-                case 'STOP':
-                    return {...state, currState: 'idle'};
-                default:
-                    return state;
-            }
-        case 'paused':
-            switch (action.type) {
-                case 'RESUME':
-                    return { ...state, currState: 'running' };
-                case 'STOP':
-                    return { ...state, currState: 'idle' };
-                default:
-                    return state;
-            }
+  switch (state.currState) {
+    case "idle":
+      switch (action.type) {
+        case "START":
+          return { ...state, currState: "running" };
         default:
-            throw new Error('Invalid State');
-    }
-}
+          return state;
+      }
+    case "running":
+      switch (action.type) {
+        case "PAUSE":
+          return { ...state, currState: "paused" };
+        case "STOP":
+          return { ...state, currState: "idle" };
+        default:
+          return state;
+      }
+    case "paused":
+      switch (action.type) {
+        case "RESUME":
+          return { ...state, currState: "running" };
+        case "STOP":
+          return { ...state, currState: "idle" };
+        default:
+          return state;
+      }
+    default:
+      throw new Error("Invalid State");
+  }
+};
